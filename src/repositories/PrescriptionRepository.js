7a7c6d5d60982f10f13969ba5426de468ce5495a
sadeepaZincat <sadeepa.zincat@gmail.com>
2026-08-15 22:28:08 +0530

    updated the prescription Specs

--- src/repositories/PrescriptionRepository.js
+++ src/repositories/PrescriptionRepository.js
@@ -1,6 +1,50 @@
 const db = require('../config/database');
 
 class PrescriptionRepository {
+  constructor() {
+    this.initSchema();
+  }
+
+  async initSchema() {
+    try {
+      const [cols] = await db.query("SHOW COLUMNS FROM prescriptions");
+      const colNames = cols.map((c) => c.Field);
+
+      const columnsToAdd = [
+        { name: 'od_va', type: "VARCHAR(20) DEFAULT '6/6'" },
+        { name: 'os_va', type: "VARCHAR(20) DEFAULT '6/6'" },
+        { name: 'od_add', type: "DECIMAL(5,2) DEFAULT 0.00" },
+        { name: 'os_add', type: "DECIMAL(5,2) DEFAULT 0.00" },
+        { name: 'pd_right', type: "DECIMAL(5,2) NULL" },
+        { name: 'pd_left', type: "DECIMAL(5,2) NULL" },
+        { name: 'pd_near', type: "DECIMAL(5,2) NULL" },
+        { name: 'pd_near_right', type: "DECIMAL(5,2) NULL" },
+        { name: 'pd_near_left', type: "DECIMAL(5,2) NULL" },
+        { name: 'fh_right', type: "DECIMAL(5,2) NULL" },
+        { name: 'fh_left', type: "DECIMAL(5,2) NULL" },
+        { name: 'sh_right', type: "DECIMAL(5,2) NULL" },
+        { name: 'sh_left', type: "DECIMAL(5,2) NULL" },
+        { name: 'a_val', type: "VARCHAR(20) NULL" },
+        { name: 'b_val', type: "VARCHAR(20) NULL" },
+        { name: 'dbl_val', type: "VARCHAR(20) NULL" },
+        { name: 'dia_right', type: "VARCHAR(20) NULL" },
+        { name: 'dia_left', type: "VARCHAR(20) NULL" },
+        { name: 'base_curve_right', type: "VARCHAR(20) NULL" },
+        { name: 'base_curve_left', type: "VARCHAR(20) NULL" },
+        { name: 'panto_angle', type: "VARCHAR(20) NULL" },
+        { name: 'wrap_angle', type: "VARCHAR(20) NULL" }
+      ];
+
+      for (const col of columnsToAdd) {
+        if (!colNames.includes(col.name)) {
+          await db.query(`ALTER TABLE prescriptions ADD COLUMN ${col.name} ${col.type}`);
+        }
+      }
+    } catch (err) {
+      console.error('Error verifying prescriptions table schema:', err.message);
+    }
+  }
+
   async generateNextPrescriptionNumber(dateStr) {
     const d = dateStr ? new Date(dateStr) : new Date();
     const yy = d.getFullYear().toString().slice(-2);
@@ -43,9 +87,12 @@ class PrescriptionRepository {
   async create(prescription) {
     const {
       branchId, customerId, optometristId, prescriptionDate, expiryDate,
-      od_sph, od_cyl, od_axis, od_add, od_prism, od_base,
-      os_sph, os_cyl, os_axis, os_add, os_prism, os_base,
-      pd, intermediateAdd, nearPd, fittingHeight, segmentHeight, remarks, prescriptionType
+      od_sph, od_cyl, od_axis, od_add, od_va, od_prism, od_base,
+      os_sph, os_cyl, os_axis, os_add, os_va, os_prism, os_base,
+      pd, pd_right, pd_left, pd_near, pd_near_right, pd_near_left,
+      intermediateAdd, nearPd, fittingHeight, segmentHeight, fh_right, fh_left, sh_right, sh_left,
+      a_val, b_val, dbl_val, dia_right, dia_left, base_curve_right, base_curve_left, panto_angle, wrap_angle,
+      remarks, prescriptionType
     } = prescription;
 
     const prescriptionNumber = prescription.prescriptionNumber || await this.generateNextPrescriptionNumber(prescriptionDate);
@@ -53,15 +100,21 @@ class PrescriptionRepository {
     const [result] = await db.query(
       `INSERT INTO prescriptions 
        (prescription_number, branch_id, customer_id, optometrist_id, prescription_date, expiry_date,
-        od_sph, od_cyl, od_axis, od_add, od_prism, od_base,
-        os_sph, os_cyl, os_axis, os_add, os_prism, os_base,
-        pd, intermediate_add, near_pd, fitting_height, segment_height, remarks, prescription_type) 
-       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+        od_sph, od_cyl, od_axis, od_add, od_va, od_prism, od_base,
+        os_sph, os_cyl, os_axis, os_add, os_va, os_prism, os_base,
+        pd, pd_right, pd_left, pd_near, pd_near_right, pd_near_left,
+        intermediate_add, near_pd, fitting_height, segment_height, fh_right, fh_left, sh_right, sh_left,
+        a_val, b_val, dbl_val, dia_right, dia_left, base_curve_right, base_curve_left, panto_angle, wrap_angle,
+        remarks, prescription_type) 
+       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
       [
         prescriptionNumber, branchId, customerId, optometristId || null, prescriptionDate, expiryDate || null,
-        od_sph !== undefined ? od_sph : 0.00, od_cyl !== undefined ? od_cyl : 0.00, od_axis || 0, od_add !== undefined ? od_add : 0.00, od_prism !== undefined ? od_prism : 0.00, od_base || null,
-        os_sph !== undefined ? os_sph : 0.00, os_cyl !== undefined ? os_cyl : 0.00, os_axis || 0, os_add !== undefined ? os_add : 0.00, os_prism !== undefined ? os_prism : 0.00, os_base || null,
-        pd || 62.00, intermediateAdd !== undefined ? intermediateAdd : 0.00, nearPd !== undefined ? nearPd : 0.00, fittingHeight !== undefined ? fittingHeight : null, segmentHeight !== undefined ? segmentHeight : null, remarks || null, prescriptionType || 'single'
+        od_sph !== undefined ? od_sph : 0.00, od_cyl !== undefined ? od_cyl : 0.00, od_axis || 0, od_add !== undefined ? od_add : 0.00, od_va || '6/6', od_prism !== undefined ? od_prism : 0.00, od_base || null,
+        os_sph !== undefined ? os_sph : 0.00, os_cyl !== undefined ? os_cyl : 0.00, os_axis || 0, os_add !== undefined ? os_add : 0.00, os_va || '6/6', os_prism !== undefined ? os_prism : 0.00, os_base || null,
+        pd || 62.00, pd_right !== undefined ? pd_right : null, pd_left !== undefined ? pd_left : null, pd_near !== undefined ? pd_near : null, pd_near_right !== undefined ? pd_near_right : null, pd_near_left !== undefined ? pd_near_left : null,
+        intermediateAdd !== undefined ? intermediateAdd : 0.00, nearPd !== undefined ? nearPd : 0.00, fittingHeight !== undefined ? fittingHeight : null, segmentHeight !== undefined ? segmentHeight : null, fh_right !== undefined ? fh_right : null, fh_left !== undefined ? fh_left : null, sh_right !== undefined ? sh_right : null, sh_left !== undefined ? sh_left : null,
+        a_val || null, b_val || null, dbl_val || null, dia_right || null, dia_left || null, base_curve_right || null, base_curve_left || null, panto_angle || null, wrap_angle || null,
+        remarks || null, prescriptionType || 'single'
       ]
     );
 
@@ -71,23 +124,32 @@ class PrescriptionRepository {
   async update(id, prescription) {
     const {
       prescriptionDate, expiryDate,
-      od_sph, od_cyl, od_axis, od_add, od_prism, od_base,
-      os_sph, os_cyl, os_axis, os_add, os_prism, os_base,
-      pd, intermediateAdd, nearPd, fittingHeight, segmentHeight, remarks, prescriptionType
+      od_sph, od_cyl, od_axis, od_add, od_va, od_prism, od_base,
+      os_sph, os_cyl, os_axis, os_add, os_va, os_prism, os_base,
+      pd, pd_right, pd_left, pd_near, pd_near_right, pd_near_left,
+      intermediateAdd, nearPd, fittingHeight, segmentHeight, fh_right, fh_left, sh_right, sh_left,
+      a_val, b_val, dbl_val, dia_right, dia_left, base_curve_right, base_curve_left, panto_angle, wrap_angle,
+      remarks, prescriptionType
     } = prescription;
 
     await db.query(
       `UPDATE prescriptions 
        SET prescription_date = ?, expiry_date = ?,
-           od_sph = ?, od_cyl = ?, od_axis = ?, od_add = ?, od_prism = ?, od_base = ?,
-           os_sph = ?, os_cyl = ?, os_axis = ?, os_add = ?, os_prism = ?, os_base = ?,
-           pd = ?, intermediate_add = ?, near_pd = ?, fitting_height = ?, segment_height = ?, remarks = ?, prescription_type = ? 
+           od_sph = ?, od_cyl = ?, od_axis = ?, od_add = ?, od_va = ?, od_prism = ?, od_base = ?,
+           os_sph = ?, os_cyl = ?, os_axis = ?, os_add = ?, os_va = ?, os_prism = ?, os_base = ?,
+           pd = ?, pd_right = ?, pd_left = ?, pd_near = ?, pd_near_right = ?, pd_near_left = ?,
+           intermediate_add = ?, near_pd = ?, fitting_height = ?, segment_height = ?, fh_right = ?, fh_left = ?, sh_right = ?, sh_left = ?,
+           a_val = ?, b_val = ?, dbl_val = ?, dia_right = ?, dia_left = ?, base_curve_right = ?, base_curve_left = ?, panto_angle = ?, wrap_angle = ?,
+           remarks = ?, prescription_type = ? 
        WHERE id = ?`,
       [
         prescriptionDate, expiryDate || null,
-        od_sph, od_cyl, od_axis, od_add, od_prism, od_base,
-        os_sph, os_cyl, os_axis, os_add, os_prism, os_base,
-        pd, intermediateAdd, nearPd, fittingHeight !== undefined ? fittingHeight : null, segmentHeight !== undefined ? segmentHeight : null, remarks, prescriptionType, id
+        od_sph, od_cyl, od_axis, od_add, od_va || '6/6', od_prism, od_base,
+        os_sph, os_cyl, os_axis, os_add, os_va || '6/6', os_prism, os_base,
+        pd, pd_right !== undefined ? pd_right : null, pd_left !== undefined ? pd_left : null, pd_near !== undefined ? pd_near : null, pd_near_right !== undefined ? pd_near_right : null, pd_near_left !== undefined ? pd_near_left : null,
+        intermediateAdd, nearPd, fittingHeight !== undefined ? fittingHeight : null, segmentHeight !== undefined ? segmentHeight : null, fh_right !== undefined ? fh_right : null, fh_left !== undefined ? fh_left : null, sh_right !== undefined ? sh_right : null, sh_left !== undefined ? sh_left : null,
+        a_val || null, b_val || null, dbl_val || null, dia_right || null, dia_left || null, base_curve_right || null, base_curve_left || null, panto_angle || null, wrap_angle || null,
+        remarks, prescriptionType, id
       ]
     );
 
